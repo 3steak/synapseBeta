c6e09a0d8a95b42d3034ec12f76e036d2858cf13
3steak <cyprien.bocquet@gmail.com>
2025-04-15 10:37:46 +0200

    ajout spatie roles et permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
+    use HasRoles;
+
 
     protected $fillable = [
         'name',
